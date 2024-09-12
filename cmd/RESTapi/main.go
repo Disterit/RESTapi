66386f7afb0d86bce141a422e3f23cf7ed73753c
Dister <Levchikov.zhenya@mail.ru>
2024-09-12 13:22:00 +0700

    Create config

---
+++ cmd/RESTapi/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"RESTapi/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting api wallet", slog.String("key", cfg.Env))
+	log.Debug("debug message are enable")
+
+}
+
+func setupLogger(env string) *slog.Logger {
+
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
