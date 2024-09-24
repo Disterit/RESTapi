9c22fcd320c4032c419046fb587a1f9488a5f2dd
Dister <Levchikov.zhenya@mail.ru>
2024-09-24 14:44:31 +0700

    creation test for api and query

--- internal/api/api_test.go
+++ internal/api/api_test.go
@@ -1,1 +1,211 @@
 package api
+
+import (
+	"RESTapi/internal/storage/postgres"
+	"bytes"
+	"encoding/json"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-chi/chi"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestCreateWalletHandler(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("unexpected error when opening a stub database connection: %s", err)
+	}
+	defer db.Close()
+
+	storage := postgres.NewStorage(db)
+
+	mock.ExpectQuery("insert into wallet \\(balance\\) values \\(100\\) returning id, balance").
+		WillReturnRows(sqlmock.NewRows([]string{"id", "balance"}).AddRow(1, 100.0))
+
+	r := chi.NewRouter()
+
+	r.Post("/wallet", CreateWalletHandler(storage))
+
+	req, err := http.NewRequest("POST", "/wallet", nil)
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	r.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected status OK, got %v", rr.Code)
+	}
+
+	var wallet postgres.Wallet
+	err = json.Unmarshal(rr.Body.Bytes(), &wallet)
+	if err != nil {
+		t.Fatalf("could not unmarshal response body: %v", err)
+	}
+
+	if wallet.ID != 1 || wallet.Balance != 100.0 {
+		t.Errorf("unexpected wallet data: got %+v, want {ID: 1, Balance: 100.0}", wallet)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestSendMoneyHandler(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("unexpected error when opening a stub database connection: %s", err)
+	}
+	defer db.Close()
+
+	storage := postgres.NewStorage(db)
+
+	transfer := Transfer{
+		Amount: 50.0,
+		ToID:   2,
+	}
+
+	transferData, err := json.Marshal(transfer)
+	if err != nil {
+		t.Fatalf("could not marshal transfer data: %v", err)
+	}
+
+	mock.ExpectBegin()
+	mock.ExpectExec(`UPDATE wallet SET balance = balance - \$1 WHERE id = \$2 AND balance >= \$1`).
+		WithArgs(transfer.Amount, 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectExec(`UPDATE wallet SET balance = balance \+ \$1 WHERE id = \$2`).
+		WithArgs(transfer.Amount, transfer.ToID).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectExec(`insert into history \(fromID, toID, time, amount\)`).
+		WithArgs(1, transfer.ToID, sqlmock.AnyArg(), transfer.Amount).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectCommit()
+
+	r := chi.NewRouter()
+	r.Post("/check/v1/wallet/{id}/send", SendMoneyHandler(storage))
+
+	// Create a request
+	req, err := http.NewRequest("POST", "/check/v1/wallet/1/send", bytes.NewBuffer(transferData))
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected status OK, got %v", rr.Code)
+	}
+}
+
+func TestHistoryWalletHandler(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("unexpected error when opening a stub database connection: %s", err)
+	}
+	defer db.Close()
+
+	storage := postgres.NewStorage(db)
+
+	// Задать фиксированное время
+	fixedTime := time.Date(2024, 9, 24, 14, 37, 29, 0, time.UTC)
+
+	walletHistory := []postgres.WalletHistory{
+		{From: 1, To: 2, Time: fixedTime, Amount: 50.0},
+		{From: 1, To: 3, Time: fixedTime, Amount: 30.0},
+	}
+
+	// Мокируем ожидание вызова функции HistoryWallet
+	mock.ExpectQuery(`SELECT fromid, toid, time, amount FROM history WHERE fromid = \$1`).
+		WithArgs(1).
+		WillReturnRows(sqlmock.NewRows([]string{"fromid", "toid", "time", "amount"}).
+			AddRow(walletHistory[0].From, walletHistory[0].To, walletHistory[0].Time, walletHistory[0].Amount).
+			AddRow(walletHistory[1].From, walletHistory[1].To, walletHistory[1].Time, walletHistory[1].Amount))
+
+	r := chi.NewRouter()
+	r.Get("/check/v1/wallet/{id}/history", HistoryWalletHandler(storage))
+
+	req, err := http.NewRequest("GET", "/check/v1/wallet/1/history", nil)
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected status OK, got %v", rr.Code)
+	}
+
+	expectedResponse, err := json.Marshal(walletHistory)
+	if err != nil {
+		t.Fatalf("could not marshal expected response: %v", err)
+	}
+
+	if rr.Body.String() != string(expectedResponse) {
+		t.Errorf("expected response body %s, got %s", expectedResponse, rr.Body.String())
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestInfoWalletHandler(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("unexpected error when opening a stub database connection: %s", err)
+	}
+	defer db.Close()
+
+	storage := postgres.NewStorage(db)
+
+	walletID := 1
+	expectedWallet := postgres.Wallet{
+		ID:      walletID,
+		Balance: 100.0,
+	}
+
+	mock.ExpectQuery(`select \* from wallet where id = \$1`).
+		WithArgs(walletID).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "balance"}).
+			AddRow(expectedWallet.ID, expectedWallet.Balance))
+
+	r := chi.NewRouter()
+	r.Get("/check/v1/wallet/{id}", InfoWalletHandler(storage))
+
+	req, err := http.NewRequest("GET", "/check/v1/wallet/"+strconv.Itoa(walletID), nil)
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+
+	expectedResponse, err := json.Marshal(expectedWallet)
+	if err != nil {
+		t.Fatalf("could not marshal expected response: %v", err)
+	}
+
+	assert.JSONEq(t, string(expectedResponse), rr.Body.String())
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
--- internal/storage/postgres/postgres_test.go
+++ internal/storage/postgres/postgres_test.go
@@ -2,22 +2,22 @@ package postgres
 
 import (
 	"github.com/DATA-DOG/go-sqlmock"
+	"net/http"
 	"testing"
+	"time"
 )
 
 func TestCreateWallet(t *testing.T) {
-	// Создаем мок для sql.DB
+
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("unexpected error when opening a stub database connection: %s", err)
 	}
 	defer db.Close()
 
-	// Эмулируем возвращаемый результат для запроса
 	rows := sqlmock.NewRows([]string{"id", "balance"}).AddRow(1, 100)
 	mock.ExpectQuery("insert into wallet").WillReturnRows(rows)
 
-	// Создаем storage с моком базы данных
 	storage := NewStorage(db)
 
 	wallet, err := storage.CreateWallet()
@@ -25,12 +25,170 @@ func TestCreateWallet(t *testing.T) {
 		t.Errorf("unexpected error: %s", err)
 	}
 
-	// Проверяем результат
 	if wallet.ID != 1 || wallet.Balance != 100 {
-		t.Errorf("unexpected wallet data: got ID %d, Balance %d", wallet.ID, wallet.Balance)
+		t.Errorf("unexpected wallet data: got ID %d, Balance %f", wallet.ID, wallet.Balance)
 	}
 
-	// Убеждаемся, что все ожидаемые запросы были выполнены
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestTransferMoney(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("unexpected error when opening a stub database connection: %s", err)
+	}
+	defer db.Close()
+
+	storage := NewStorage(db)
+
+	amount := float32(50)
+	walletID := 1
+	walletToID := 2
+
+	mock.ExpectBegin()
+
+	mock.ExpectExec("UPDATE wallet SET balance = balance -").
+		WithArgs(amount, walletID).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectExec("UPDATE wallet SET balance = balance +").
+		WithArgs(amount, walletToID).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectCommit()
+
+	status, err := storage.TransferMoney(amount, walletID, walletToID)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	if status != http.StatusOK {
+		t.Errorf("unexpected status: got %v, want %v", status, http.StatusOK)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestGetWalletInfo(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("unexpected error when opening a stub database connection: %s", err)
+	}
+	defer db.Close()
+
+	storage := NewStorage(db)
+
+	walletID := 1
+	expectedWallet := Wallet{
+		ID:      walletID,
+		Balance: 100.0,
+	}
+
+	rows := sqlmock.NewRows([]string{"id", "balance"}).AddRow(expectedWallet.ID, expectedWallet.Balance)
+	mock.ExpectQuery("select \\* from wallet where id = \\$1").
+		WithArgs(walletID).
+		WillReturnRows(rows)
+
+	wallet, err := storage.GetWalletInfo(walletID)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	if wallet.ID != expectedWallet.ID || wallet.Balance != expectedWallet.Balance {
+		t.Errorf("unexpected wallet info: got %+v, want %+v", wallet, expectedWallet)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestHistoryWallet(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("unexpected error when opening a stub database connection: %s", err)
+	}
+	defer db.Close()
+
+	storage := NewStorage(db)
+
+	walletID := 1
+	expectedHistory := []WalletHistory{
+		{
+			From:   walletID,
+			To:     2,
+			Time:   time.Now(),
+			Amount: 100.0,
+		},
+		{
+			From:   walletID,
+			To:     3,
+			Time:   time.Now(),
+			Amount: 50.0,
+		},
+	}
+
+	rows := sqlmock.NewRows([]string{"fromid", "toid", "time", "amount"}).
+		AddRow(expectedHistory[0].From, expectedHistory[0].To, expectedHistory[0].Time, expectedHistory[0].Amount).
+		AddRow(expectedHistory[1].From, expectedHistory[1].To, expectedHistory[1].Time, expectedHistory[1].Amount)
+
+	mock.ExpectQuery("SELECT fromid, toid, time, amount FROM history WHERE fromid = \\$1").
+		WithArgs(walletID).
+		WillReturnRows(rows)
+
+	history, status := storage.HistoryWallet(walletID)
+	if status != http.StatusOK {
+		t.Errorf("unexpected status: got %v, want %v", status, http.StatusOK)
+	}
+
+	if len(history) != len(expectedHistory) {
+		t.Fatalf("unexpected length of history: got %d, want %d", len(history), len(expectedHistory))
+	}
+	for i := range history {
+		if history[i].From != expectedHistory[i].From ||
+			history[i].To != expectedHistory[i].To ||
+			history[i].Amount != expectedHistory[i].Amount ||
+			!history[i].Time.Equal(expectedHistory[i].Time) {
+			t.Errorf("unexpected history entry at index %d: got %+v, want %+v", i, history[i], expectedHistory[i])
+		}
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestAddHistoryWallet(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("unexpected error when opening a stub database connection: %s", err)
+	}
+	defer db.Close()
+
+	storage := NewStorage(db)
+
+	date := time.Now()
+	fromID := 1
+	toID := 2
+	amount := float32(100.0)
+
+	mock.ExpectExec("insert into history \\(fromID, toID, time, amount\\) values \\(\\$1, \\$2, \\$3, \\$4\\)").
+		WithArgs(fromID, toID, date, amount).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err = storage.AddHistoryWallet(date, fromID, toID, amount)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
